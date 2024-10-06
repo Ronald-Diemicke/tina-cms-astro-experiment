3e99334a38d91a4421b2f23866fca969289cac20
Ronald Diemicke <ron.diemicke@gm.com>
2024-10-06 13:44:57 -0500

    Blocks are now connected to the contextual editor

--- src/components/sampleBlocksSection.tsx
+++ src/components/sampleBlocksSection.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import type {PageQuery, PageQueryVariables} from '../../tina/__generated__/types'
 import SampleBlock from './sampleBlock.tsx'
+import { useTina, tinaField } from "tinacms/dist/react";
 
 interface Props {
     tinaProps: {
@@ -11,16 +12,24 @@ interface Props {
 }
 
 export default function sampleBlocksSection ({ tinaProps }: Props) {
+    const { data } = useTina({
+        variables: tinaProps.variables,
+        data: tinaProps.data,
+        query: tinaProps.query
+    });
+
+    const { page} = data;
+
     return (
         <>
             <div>block section</div>
-            {tinaProps.data.page.sampleBlocksSection
-                ? tinaProps.data.page.sampleBlocksSection.map(function (blockData, i) {
+            {page.sampleBlocksSection
+                ? page.sampleBlocksSection.map(function (blockData, i) {
                     switch (blockData.__typename) {
                         case 'PageSampleBlocksSectionSampleBlock':
                             return (
                                 <React.Fragment key={i + blockData.__typename}>
-                                    <SampleBlock tinaProps={blockData}/>
+                                    <SampleBlock data-tina-field={tinaField(page, 'sampleBlock')} tinaProps={blockData}/>
                                 </React.Fragment>
                             )
                         default:
